83d9c58c6709226e7b5e87579e1ace795401ff0b
posumah08 <posumah08@gmail.com>
2026-04-21 21:54:50 +0700

    questions.py

--- questions.py
+++ questions.py
@@ -2,8 +2,10 @@ import random
 
 QUESTIONS = [
     {"question": "Hero lifesteal tinggi?", "options": ["Alucard","Layla","Miya","Eudora"], "answer": 0},
-    {"question": "Ultimate Harith?", "options": ["Chrono Dash","Zaman Force","Black Shoes","Time Rift"], "answer": 2}
+    {"question": "Ultimate Harith?", "options": ["Chrono Dash","Zaman Force","Black Shoes","Time Rift"], "answer": 2},
+    {"question": "Hero tank MLBB?", "options": ["Tigreal","Layla","Gusion","Fanny"], "answer": 0},
+    {"question": "Hero assassin?", "options": ["Miya","Saber","Estes","Franco"], "answer": 1}
 ]
 
 def get_questions():
-    return random.sample(QUESTIONS, len(QUESTIONS))
+    return random.sample(QUESTIONS, len(QUESTIONS))  # 🔥 
