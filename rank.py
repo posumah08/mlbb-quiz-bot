621a71839e490b66ce552a151c34ffdb5ed026db
posumah08 <posumah08@gmail.com>
2026-04-24 17:08:05 +0700

    Update rank.py

--- rank.py
+++ rank.py
@@ -1,10 +1,14 @@
 def get_rank(mmr):
     if mmr <= 2000:
         return "EPIC"
-    elif mmr <= 4000:
+    elif mmr <= 5000:
         return "LEGEND"
+    elif mmr <= 7500:
+        return "MYTHIC"
     elif mmr <= 10000:
-        return "HONOR"
+        return "MYTHIC HONOR"
+    elif mmr <= 15000:
+        return "MYTHIC GLORY"
     else:
         return "IMMORTAL"
 
@@ -14,7 +18,11 @@ def get_rank_emoji(rank):
         return "<tg-emoji emoji-id='5436186359050030684'></tg-emoji>"
     elif rank == "LEGEND":
         return "<tg-emoji emoji-id='5436072593956294076'></tg-emoji>"
-    elif rank == "HONOR":
+    elif rank == "MYTHIC":
+        return "<tg-emoji emoji-id='5440505811954587601'></tg-emoji>"
+    elif rank == "MYTHIC HONOR":
+        return "<tg-emoji emoji-id='5440505811954587601'></tg-emoji>"
+    elif rank == "MYTHIC GLORY":
         return "<tg-emoji emoji-id='5440505811954587601'></tg-emoji>"
     else:
         return "<tg-emoji emoji-id='5434039403683009050'></tg-emoji>"
