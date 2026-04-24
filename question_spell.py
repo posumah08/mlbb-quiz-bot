9e83295d7b5cfdbc2da48443f856a0f94c921ccf
posumah08 <posumah08@gmail.com>
2026-04-24 17:53:54 +0700

    Update question_spell.py

--- question_spell.py
+++ question_spell.py
@@ -1,14 +1,14 @@
-QUESTIONS_SPELL = [
-    {"image": "images/spell/Aegis.png", "answer": "Aegis", "type": "spell"},
-    {"image": "images/spell/Arrival.png", "answer": "Arrival", "type": "spell"},
-    {"image": "images/spell/Execute.png", "answer": "Execute", "type": "spell"},
-    {"image": "images/spell/Flameshot.png", "answer": "Flameshot", "type": "spell"},
-    {"image": "images/spell/Flicker.png", "answer": "Flicker", "type": "spell"},
-    {"image": "images/spell/Inspire.png", "answer": "Inspire", "type": "spell"},
-    {"image": "images/spell/Petrify.png", "answer": "Petrify", "type": "spell"},
-    {"image": "images/spell/Purify.png", "answer": "Purify", "type": "spell"},
-    {"image": "images/spell/Retribution.png", "answer": "Retribution", "type": "spell"},
-    {"image": "images/spell/Revitalize.png", "answer": "Revitalize", "type": "spell"},
-    {"image": "images/spell/Sprint.png", "answer": "Sprint", "type": "spell"},
-    {"image": "images/spell/Vengeance.png", "answer": "Vengeance", "type": "spell"},
+QUESTIONS = [
+    {"image": "images/spell/Aegis.png", "answer": "Aegis"},
+    {"image": "images/spell/Arrival.png", "answer": "Arrival"},
+    {"image": "images/spell/Execute.png", "answer": "Execute"},
+    {"image": "images/spell/Flameshot.png", "answer": "Flameshot"},
+    {"image": "images/spell/Flicker.png", "answer": "Flicker"},
+    {"image": "images/spell/Inspire.png", "answer": "Inspire"},
+    {"image": "images/spell/Petrify.png", "answer": "Petrify"},
+    {"image": "images/spell/Purify.png", "answer": "Purify"},
+    {"image": "images/spell/Retribution.png", "answer": "Retribution"},
+    {"image": "images/spell/Revitalize.png", "answer": "Revitalize"},
+    {"image": "images/spell/Sprint.png", "answer": "Sprint"},
+    {"image": "images/spell/Vengeance.png", "answer": "Vengeance"}
 ]
